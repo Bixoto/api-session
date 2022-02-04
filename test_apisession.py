3bd18622d5d50059b926a1d6ef0cfc023cc8f009
Baptiste Fontaine <baptiste@bixoto.com>
2022-02-04 17:24:46 +0100

    Add one more test

--- test_apisession.py
+++ test_apisession.py
@@ -37,8 +37,19 @@ def test_raise_for_response():
 def test_throw():
     s = APISession("https://httpbin.org")
 
-    for method in (s.get_api, s.post_api, s.put_api, s.patch_api, s.delete_api):
+    for method in (s.get_api, s.post_api, s.put_api, s.patch_api, s.delete_api, s.head_api):
         with pytest.raises(HTTPError):
             method("/status/403", throw=True)
 
         assert method("/status/200", throw=True).ok
+
+
+def test_get_json_api():
+    s = APISession("https://httpbin.org")
+
+    with pytest.raises(HTTPError):
+        s.get_json_api("/status/404", none_on_404=False)
+
+    assert s.get_json_api("/status/404", none_on_404=True) is None
+
+    assert isinstance(s.get_json_api("/headers"), dict)
